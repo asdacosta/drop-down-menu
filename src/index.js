a9d40d976e043b53eacc183c6d316ac304f90128
asdacosta <asdacosta18@gmail.com>
2023-11-28 10:56:20 +0000

    Style menu in nav

--- src/index.js
+++ src/index.js
@@ -23,12 +23,37 @@ const openMenu = (function () {
 })();
 
 const closedMenu = (function () {
-  const openedMenu = document.querySelectorAll("nav > section:first-child > *");
+  const openedMenu = document.querySelectorAll(
+    "nav > section:first-child > div > *",
+  );
 
   openedMenu.forEach((element) => {
     element.addEventListener("click", () => {
       openMenu.nav.style.display = "none";
       openMenu.closedMenu.style.display = "block";
     });
+  });
+})();
+
+const styleMenu = (function () {
+  const menuSpan = document.querySelector("nav > section:first-child span");
+  const menuImg = document.querySelector("nav > section:first-child img");
+
+  menuSpan.addEventListener("mouseover", () => {
+    menuSpan.style.color = "rgb(97, 73, 8)";
+    menuImg.style.backgroundColor = "rgba(201, 150, 23, 0.8)";
+  });
+  menuSpan.addEventListener("mouseout", () => {
+    menuSpan.style.color = "black";
+    menuImg.style.backgroundColor = "rgb(201, 150, 23)";
+  });
+
+  menuImg.addEventListener("mouseover", () => {
+    menuSpan.style.color = "rgb(97, 73, 8)";
+    menuImg.style.backgroundColor = "rgba(201, 150, 23, 0.8)";
+  });
+  menuImg.addEventListener("mouseout", () => {
+    menuSpan.style.color = "black";
+    menuImg.style.backgroundColor = "rgb(201, 150, 23)";
   });
 })();

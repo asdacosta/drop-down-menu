1c07b1848d057545c640d6fdfb1ca645d46fe200
asdacosta <asdacosta18@gmail.com>
2023-11-28 08:13:50 +0000

    Add opening and closing of menu functions

--- src/index.js
+++ src/index.js
@@ -1,9 +1,34 @@
-import './style.css';
+import "./style.css";
 
 const importAllImages = (function () {
-    function importAll(r) {
-        return r.keys().map(r);
-    }
+  function importAll(r) {
+    return r.keys().map(r);
+  }
 
-    const imgs = importAll(require.context('./imgs', false, /\.(png|jpe?g|svg)$/));
+  const imgs = importAll(
+    require.context("./imgs", false, /\.(png|jpe?g|svg)$/),
+  );
+})();
+
+const openMenu = (function () {
+  const closedMenu = document.querySelector("body > div");
+  const nav = document.querySelector("nav");
+
+  closedMenu.addEventListener("click", () => {
+    closedMenu.style.display = "none";
+    nav.style.display = "flex";
+  });
+
+  return { closedMenu, nav };
+})();
+
+const closedMenu = (function () {
+  const openedMenu = document.querySelectorAll("nav > section:first-child > *");
+
+  openedMenu.forEach((element) => {
+    element.addEventListener("click", () => {
+      openMenu.nav.style.display = "none";
+      openMenu.closedMenu.style.display = "block";
+    });
+  });
 })();
